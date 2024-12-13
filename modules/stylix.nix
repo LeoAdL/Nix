16573c81b197bed9e3aba6e915e8744a54f826dc
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2024-12-12 22:25:32 -0500

    Stylix

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -2,11 +2,34 @@
 {
   stylix = {
     enable = true;
+    autoEnable = true;
     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.lato;
+        name = "Lato";
+      };
+
+      sansSerif = {
+        package = pkgs.iosevka;
+        name = "Iosevka Sans";
+      };
+
+      monospace = {
+        package = pkgs.iosevka;
+        name = "Iosevka Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+
   };
 }
